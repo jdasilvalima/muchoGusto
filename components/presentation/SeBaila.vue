4a1020710de7b740e38ec8c2b3c9c749a3e2ca0a
jade <jade.dasilva.lima.jds@gmail.com>
2023-10-15 04:10:35 -0400

    test assets deployment

--- components/presentation/SeBaila.vue
+++ components/presentation/SeBaila.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="se-come green-theme">
    <p>Se baila</p>
-   <img src="@/assets/img/sebaila-img.png" alt="People dansing image" width="500">
+   <img src="~assets/img/sebaila-img.png" alt="People dansing image" width="500">
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['@/assets/mainStyle.css'],
+  css: ['~/assets/mainStyle.css'],
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
       appDomain: "/muchoGusto/",
     }
+  },
+  router: {
+    base: '/muchoGusto/'
   }
 })
