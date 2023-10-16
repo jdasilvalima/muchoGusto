5f4812fde8da6e01015d8439e4553064bda4f34f
jade <jade.dasilva.lima.jds@gmail.com>
2023-10-16 02:37:06 -0400

    test deployment vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  head: {
-    title: 'muchoGusto',
-    htmlAttrs: {
-      lang: 'en',
-    },
-  },
-
   css: ['~/assets/mainStyle.css'],
   devtools: { enabled: true },
 
-  app: {
-    baseURL: '/muchoGusto/',
-    buildAssetsDir: '/_nuxt/',
-  },
-
   runtimeConfig: {
     public: {
-      domain: '/muchoGusto/',
+      domain: '/mucho-gusto/',
     }
   },
 })
