52b44bd52e0438f40880d6a9bd77a166d06b3c01
jade <jade.dasilva.lima.jds@gmail.com>
2023-11-11 06:19:02 -0500

    resize h1 while scrolling

--- components/header/Hero.vue
+++ components/header/Hero.vue
@@ -5,9 +5,29 @@
       <p>Paris Market</p>
       <p>Paris</p>
     </div>
-   <h1>muchoGusto</h1>
+   <h1 :style="{'font-size': fontSize + 'rem'}">muchoGusto</h1>
  </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const initialFontSize = 14.5;
+let fontSize = ref<number>(initialFontSize);
+
+onMounted(() => {
+  window.addEventListener('scroll', modifyTextSize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', modifyTextSize);
+});
+
+function modifyTextSize() {
+  const currentPagePosition = window.scrollY;
+  fontSize.value = initialFontSize - (currentPagePosition * 0.01)
+}
+</script>
 
 <style scoped>
 .header-hero {
@@ -24,11 +44,10 @@
 }
 
 h1 {
-  font-size: 14.5rem;
   position: absolute;
   bottom: 1.25rem;
-  right: 1.25rem;
-  left: 1.25rem;
+  left: 50%;
+  transform: translateX(-50%);
   transform-origin: center bottom;
 }
 </style>
