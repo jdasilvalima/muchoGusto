df1fedb80f6fe92b2cfaa633c58dce4c487e82f9
jade <jade.dasilva.lima.jds@gmail.com>
2023-11-12 06:03:35 -0500

    work on text animation

--- components/header/Hero.vue
+++ components/header/Hero.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="header-hero green-theme">
     <div class="header-hero-text">
-      <p>Rooftop</p>
-      <p>Paris Market</p>
-      <p>Paris</p>
+      <p class="tex-left">Rooftop</p>
+      <p class="tex-center">Paris Market</p>
+      <p class="tex-right">Paris</p>
     </div>
-   <h1 :style="{'font-size': fontSize + 'rem'}">muchoGusto</h1>
+   <h1 :style="{'font-size': fontSize + 'vw'}">muchoGusto</h1>
  </div>
 </template>
 
@@ -33,13 +33,15 @@ function modifyTextSize() {
 .header-hero {
   font-family: 'CakoRegular', sans-serif;
   height: 100vh;
+  position: relative;
 }
 
 .header-hero-text {
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
   padding: 1rem;
-  font-size: 2rem;
+  font-size: 6vw;
   text-transform: uppercase;
 }
 
@@ -47,7 +49,56 @@ h1 {
   position: absolute;
   bottom: 1.25rem;
   left: 50%;
-  transform: translateX(-50%);
   transform-origin: center bottom;
+	animation: slide-top 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) 0s forwards;
+}
+
+.tex-center {
+  animation: slide-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) 1s both;
+}
+
+.tex-left {
+  animation: slide-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) 1.5s both;
+}
+
+.tex-right {
+  animation: slide-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) 2s both;
+}
+
+@media (min-width:650px) {
+  .header-hero-text {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    font-size: 2rem;
+  }
+}
+
+/**
+ * ----------------------------------------
+ * animation slide-top
+ * ----------------------------------------
+ */
+@keyframes slide-top {
+  0% {
+    transform: translate(-50%, 120%);
+  }
+  100% {
+    transform: translate(-50%, 0%);
+  }
+}
+
+/**
+ * ----------------------------------------
+ * animation slide-bottom
+ * ----------------------------------------
+ */
+@keyframes slide-bottom {
+  0% {
+    transform: translateY(-150%);
+  }
+  100% {
+    transform: translateY(0);
+  }
 }
 </style>
