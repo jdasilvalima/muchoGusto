c1da7ec0f16856401912b04edfd34745cf332c87
jade <jade.dasilva.lima.jds@gmail.com>
2023-11-18 04:54:37 -0500

    mouth transition

--- components/header/Hero.vue
+++ components/header/Hero.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="header-hero green-theme">
 
+    <!-- TODO handle smooth scroll -->
+    
     <div class="header-hero-text">
       <p class="tex-left">Rooftop</p>
       <p class="tex-center">Paris Market</p>
       <p class="tex-right">Paris</p>
     </div>
 
-    <!-- TODO clipPath only for mouth pink  -->
-    <div class="header-hero-mouth" :style="{ transform: `translate3d(0, ${mouthTranslateY}px, 0)`, clipPath: `polygon(0% 0%, 100% 0%, 100% ${clipPathY}vh, 0% ${clipPathY}vh)` }">
-      <div class="header-hero-mouth-svg mouth-pink">
+    <div class="header-hero-mouth" :style="{ transform: `translate3d(0, ${mouthTranslateY}px, 0)` }">
+      <div class="header-hero-mouth-svg mouth-pink" :style="{ clipPath: `polygon(0% 0%, 100% 0%, 100% ${clipPathY}vh, 0% ${clipPathY}vh)` }">
         <svg viewBox="0 0 157.7 108.2">
           <path fill="#E7BEAF" class="mouth-spot" d="M124.4,0.1c5.6-0.2,7.3,1.5,7,6.9c-0.3,4.7-2.7,7.5-7.3,8.3c-3.9,0.7-8.6-1.6-10.2-5.1c-1.1-2.3,0.1-6,2.4-7.3
               C118.8,1.5,121.2-0.5,124.4,0.1"></path>
@@ -18,13 +19,14 @@
           <path fill="#E7BEAF" class="mouth-part-bottom" d="M153.2,74.6c0,0-27.8,35.7-68.5,28.1c-25.4-4.8-72.5-26.5-72.5-26.5s87.2,11.1,131.5,0" stroke-linecap="round" stroke-linejoin="round"></path>
         </svg>
       </div>
+
       <div class="header-hero-mouth-svg mouth-green">
         <svg viewBox="0 0 157.7 108.2">
-          <path fill="#3A4638" class="mouth-spot" d="M124.4,0.1c5.6-0.2,7.3,1.5,7,6.9c-0.3,4.7-2.7,7.5-7.3,8.3c-3.9,0.7-8.6-1.6-10.2-5.1c-1.1-2.3,0.1-6,2.4-7.3
-              C118.8,1.5,121.2-0.5,124.4,0.1"></path>
-          <path fill="#3A4638" class="mouth-part-top" d="M4.5,68.2c0,0,39.8-42.7,57.6-32.4c17.8,10.3,21.4,16.3,21.4,16.3s13.8-20.3,32-14
-              c18.2,6.3,37.6,36.4,37.6,36.4c-6.8-5.1-37.5-13.5-70.9-13.9c-33.3-0.4-70.1,15.5-70.1,15.5" stroke-linecap="round" stroke-linejoin="round"></path>
-          <path fill="#3A4638" class="mouth-part-bottom" d="M153.2,74.6c0,0-27.8,35.7-68.5,28.1c-25.4-4.8-72.5-26.5-72.5-26.5s87.2,11.1,131.5,0" stroke-linecap="round" stroke-linejoin="round"></path>
+          <path fill="#3A4638" d="M124.4,0.1c5.6-0.2,7.3,1.5,7,6.9c-0.3,4.7-2.7,7.5-7.3,8.3c-3.9,0.7-8.6-1.6-10.2-5.1c-1.1-2.3,0.1-6,2.4-7.3
+            C118.8,1.5,121.2-0.5,124.4,0.1"></path>
+          <path fill="#3A4638" d="M4.5,68.2c0,0,39.8-42.7,57.6-32.4c17.8,10.3,21.4,16.3,21.4,16.3s13.8-20.3,32-14
+            c18.2,6.3,37.6,36.4,37.6,36.4c-6.8-5.1-37.5-13.5-70.9-13.9c-33.3-0.4-70.1,15.5-70.1,15.5" ></path>
+          <path fill="#3A4638" d="M153.2,74.6c0,0-27.8,35.7-68.5,28.1c-25.4-4.8-72.5-26.5-72.5-26.5s87.2,11.1,131.5,0" ></path>
         </svg>
       </div>
     </div>
@@ -58,16 +60,18 @@ function modifyTextSize() {
 }
 
 function handleScroll() {
-  mouthTranslateY.value = window.scrollY;
-  clipPathY.value = 100 - window.scrollY * 0.1;
-  console.log('clipPathY ', clipPathY.value);
+  if(window.scrollY < 1390)
+    mouthTranslateY.value = window.scrollY;
+
+  if(window.scrollY < 571.4)
+    clipPathY.value = 100 - window.scrollY * 0.175;
 }
 </script>
 
 <style scoped>
 .header-hero {
   font-family: 'CakoRegular', sans-serif;
-  height: 100vh;
+  height: 707px;
   position: relative;
 }
 
@@ -116,11 +120,11 @@ h1 {
   display: flex;
   align-items: center;
   justify-content: center;
-  z-index: 2;
 }
 
 .header-hero-mouth-svg svg {
-  margin-top: -10vh;
+  margin-top: -35vh;
+  padding: 0;
   width: 40vw;
   height: 40vh;
 }
@@ -135,6 +139,14 @@ h1 {
 
 .mouth-part-bottom {
   animation: animate-mouth-part-bottom 2s cubic-bezier(0.47, 0, 0.745, 0.715) 1s both;
+}
+
+.mouth-pink {
+  z-index: 3;
+}
+
+.mouth-green {
+  z-index: 2;
 }
 
 @media (min-width:650px) {
